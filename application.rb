c5b404705f2f14e9912a237f6c2cd7f04cb5c701
Alex Lemanski <alex.lemanski@bitfyre.net>
2012-02-05 15:56:00 -0600

    add model validations

--- application.rb
+++ application.rb
@@ -1,6 +1,11 @@
 require "sinatra"
 require "sinatra/activerecord"
 
+class TaskList < ActiveRecord::Base
+  validates_uniqueness_of :list_name
+  validates_presence_of   :list_name
+end
+
 get '/' do
   "Bitfyre's Todos"
 end
