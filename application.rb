f9270d5d90cc50fc6bb7c347f07c18bdca3af9eb
Alex Lemanski <alex.lemanski@bitfyre.net>
2012-02-06 03:27:00 -0600

    add a partial helper from Sinatra book

--- application.rb
+++ application.rb
@@ -1,6 +1,30 @@
 require "sinatra"
 require "sinatra/activerecord"
 
+# Render the page once:
+# Usage: partial :foo
+# 
+# foo will be rendered once for each element in the array, passing in a local variable named "foo"
+# Usage: partial :foo, :collection => @my_foos    
+
+helpers do
+  def partial(template, *args)
+    options = args.extract_options!
+    options.merge!(:layout => false)
+    if collection = options.delete(:collection) then
+      collection.inject([]) do |buffer, member|
+        buffer << haml(template, options.merge(
+                                  :layout => false, 
+                                  :locals => {template.to_sym => member}
+                                )
+                     )
+      end.join("\n")
+    else
+      haml(template, options)
+    end
+  end
+end
+
 class TaskList < ActiveRecord::Base
   attr_accessible :list_name
 
